34721cde151478c695ef41de8cf13a824284ad8e
Davi Almeida <davistrife@live.com>
2021-09-17 09:34:39 -0300

    feat: error handling

--- src/services/consultarRaisQueue.js
+++ src/services/consultarRaisQueue.js
@@ -3,7 +3,26 @@ const { default: axios } = require('axios')
 
 const queue = async.queue(async ({pis, apiKey}, callback) => {
     const query = "SELECT FROM 'RAIS'.'CONSULTA'"
-    await axios.get(`https://irql.icheques.com.br/?q=${query}&pis=${pis}&apiKey=${apiKey}&json=true`)
+    await axios.get(`https://irql.icheques.com.br/?q=${query}&pis=${pis}&apiKey=${apiKey}&json=true`).then(
+        sucess => console.log(`Sucesso: PIS ${pis} para apiKey ${apiKey}`)
+    ).catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
     callback()
 }, 5)
 
