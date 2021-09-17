54b5bb066fb7e4b3825d754ccb2ce123cb3ad27e
Davi Almeida <davistrife@live.com>
2021-09-17 09:14:26 -0300

    chore: add logger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const morgan = require('morgan')
+const logger = require('./providers/logger')
 const consultarRaisQueue = require('./services/consultarRaisQueue')
 
 const app = express()
 
 app.use(express.json())
+
+const stream = {
+    // Use the http severity
+    write: (message) => logger.http(message),
+}
+
+app.use(morgan(function (tokens, req, res) {
+    const data = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['user-agent'](req, res),
+    ]
+  
+    return data.join(' ')
+  }, {
+    stream
+  }))
 
 app.post('/', async (req, res) => {
     consultarRaisQueue.handle(req.body)
